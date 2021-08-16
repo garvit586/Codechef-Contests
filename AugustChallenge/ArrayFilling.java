03b52642578d975b07a6caca3fd8b6a7c40cb5bb
Lenovo <garvit.gupta_cs19@gla.ac.in>
2021-08-16 23:19:20 +0530

    ARRFILL

---
+++ AugustChallenge/ArrayFilling.java
@@ -0,0 +1,100 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class ArrayFilling {
+    static class Pair {
+        int x;
+        int y;
+      
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        public int getX() {
+            return this.x;
+        }
+        public int getY() {
+            return this.y;
+        }
+    }
+  
+    // class to define user defined conparator
+    static class Compare {
+        static void compare(ArrayList<Pair> list) {
+            // Comparator to sort the pair according to second element
+            Collections.sort(list, new Comparator<Pair>() {
+                @Override public int compare(Pair p1, Pair p2)
+                {
+                    return p1.x - p2.x;
+                }
+            });
+        }
+    }
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+ 
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+ 
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() { return Integer.parseInt(next()); }
+ 
+        long nextLong() { return Long.parseLong(next()); }
+ 
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+ 
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static long gcd(long a, long b) {
+        if (a == 0) return b;
+        return gcd(b % a, a);
+    }
+  public static void main (String[] args) throws java.lang.Exception {
+    FastReader sc = new FastReader();
+    int t = sc.nextInt();
+    while(t-- > 0) {
+        long n = sc.nextLong();
+        long m = sc.nextLong();
+        long temp = 1, ans = 0, cnt = n;
+        ArrayList<Pair> list = new ArrayList<>();
+        for(long i = 1; i <= m; i++) {
+            list.add(new Pair((int)sc.nextLong(), (int)sc.nextLong()));
+        }
+        Compare obj = new Compare();
+        obj.compare(list);
+        for(int i = (int)m-1; i > -1 && cnt > 0; i--) {
+            long x = (long)list.get(i).getX();
+            long y = (long)list.get(i).getY();
+            temp = (temp * y) / gcd(temp, y);
+            ans += (cnt - (n / temp)) * x;
+            cnt = n / temp;
+            // System.out.println("ans: " + ans);
+        }
+        System.out.println(ans);
+    }
+  }
+}
